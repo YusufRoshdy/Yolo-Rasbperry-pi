e04c09883d56ee6eff74b56cb1832da14e0bdc05
Yusuf Mesbah <yusufroshdy9@gmail.com>
2022-08-14 21:35:43 +0300

    Separate detection and server into different files

---
+++ detector.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+
+from multiprocessing import Process, Manager
+from multiprocessing.connection import Client
+
+address = ('localhost', 6000)
+conn = Client(address, authkey=b'secret password')
+
+import time
+from datetime import datetime
+import cv2
+import numpy as np
+
+import yolov5
+
+# fastestv2
+import torch
+import model.detector as model_fastest
+import utils.utils
+
+# onnx
+from YOLOv7onnx import YOLOv7
+from YOLOv7onnx.utils import class_names as yolov7_names
+
+from os.path import exists
+
+if not exists('log.txt'):
+    with open("log.txt", "w") as f:
+        L = 'Time, Xmin, Ymin, Xmax, Ymax, Confidence, Class ID, Class Name\n'
+        f.write(L)
+
+# Append-adds at last
+log_file = open("log.txt", "a")  # append mode
+#file1.close()
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+def camera_process(manager_dict):
+    while True:
+        try:
+            # print('cam')
+            cap = cv2.VideoCapture(0)
+            old_time = time.time()
+
+            # Read until video is completed
+            while (cap.isOpened()):
+                # Capture frame-by-frame
+                ret, frame = cap.read()
+                if ret != True:
+                    break
+                new_time = time.time()
+                fps = 1 / (new_time - old_time)
+                old_time = new_time
+
+                stream_model = manager_dict['stream_model']
+
+                if 'Yolov5' in stream_model:
+                    frame = yolov5_process(frame, size=stream_model[-1])
+                elif 'Yolov-Fastestv2' == stream_model:
+                    frame = yolo_fastest_process(frame, frame.shape[1], frame.shape[0])
+                else:
+                    frame = yolov7_onnx_process(frame)
+
+                log_file.flush()
+
+                fps = f'FPS:{fps: 0.2f}'
+                cv2.putText(frame, fps, (7, 70), font, 2, (0, 255, 0), 2)
+
+                manager_dict['frame'] = frame
+                # frame = cv2.imencode('.jpg', frame)[1].tobytes()
+                # yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            else:
+                frame = np.ones((512, 1024))
+                cv2.putText(frame, "Camera is not available", (7, 70), font, 2, (0, 255, 0), 2)
+                manager_dict['frame'] = frame
+        except:
+            frame = np.ones((512, 1024), dtype=np.int8) * 254
+            cv2.putText(frame, "Camera is not available", (7, 70), font, 2, (0, 255, 0), 2)
+            print(manager_dict, '************')
+            manager_dict['frame'] = frame
+
+            # frame = cv2.imencode('.jpg', frame)[1].tobytes()
+            # yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+def yolov5_process(frame, size='n'):
+    global models
+
+    model_name = f'yolov5{size}.pt'
+    # load pretrained model
+    if model_name in models:
+        model = models[model_name]
+    else:
+        import yolov5
+        model = yolov5.load(model_name)
+        # set model parameters
+        model.conf = 0.25  # NMS confidence threshold
+        model.iou = 0.45  # NMS IoU threshold
+        model.agnostic = False  # NMS class-agnostic
+        model.multi_label = False  # NMS multiple labels per box
+        model.max_det = 3  # maximum number of detections per image
+
+        models[model_name] = model
+
+    results = model(frame)
+    if len(results.pandas().xyxy[0]) > 0:
+        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        for idx, row in results.pandas().xyxy[0].iterrows():
+            line = f'{now}, {int(row.xmin):3d}, {int(row.ymin):3d}, {int(row.xmax):3d}, {int(row.ymax):3d}, {row.confidence:0.2f}, {int(row["class"]):2d}, {row["name"]}\n'
+            # print(line)
+            log_file.write(line)
+    new_frame = results.render()[0]
+    return new_frame
+
+
+def yolo_fastest_process(frame, w, h):
+    global models
+
+    model_name = f'yolovfastest.pt'
+    # load pretrained model
+    if model_name in models:
+        (model, cfg, LABEL_NAMES, device, scale_h, scale_w) = models[model_name]
+    else:
+        weights = 'modelzoo/coco2017-0.241078ap-model.pth'
+        weights = 'modelzoo/fastest1.pth'
+        data = 'data/coco.data'
+        cfg = utils.utils.load_datafile('data/coco.data')
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model = model_fastest.Detector(cfg["classes"], cfg["anchor_num"], True).to(device)
+        model.load_state_dict(torch.load(weights, map_location=device))
+
+        # sets the module in eval node
+        model.eval()
+
+        LABEL_NAMES = []
+        with open(cfg["names"], 'r') as f:
+            for line in f.readlines():
+                LABEL_NAMES.append(line.strip())
+
+        scale_h, scale_w = h / cfg["height"], w / cfg["width"]
+
+        models[model_name] = (model, cfg, LABEL_NAMES, device, scale_h, scale_w)
+
+    res_frame = cv2.resize(frame, (cfg["width"], cfg["height"]), interpolation=cv2.INTER_LINEAR)
+    temp_frame = res_frame.reshape(1, cfg["height"], cfg["width"], 3)
+    temp_frame = torch.from_numpy(temp_frame.transpose(0, 3, 1, 2))
+    temp_frame = temp_frame.to(device).float() / 255.0
+
+    preds = model(temp_frame)
+
+    output = utils.utils.handel_preds(preds, cfg, device)
+    output_boxes = utils.utils.non_max_suppression(output, conf_thres=0.3, iou_thres=0.4)
+
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    for box in output_boxes[0]:
+        box = box.tolist()
+
+        obj_score = box[4]
+        category = LABEL_NAMES[int(box[5])]
+
+        x1, y1 = int(box[0] * scale_w), int(box[1] * scale_h)
+        x2, y2 = int(box[2] * scale_w), int(box[3] * scale_h)
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 0), 2)
+        cv2.putText(frame, '%.2f' % obj_score, (x1, y1 - 5), 0, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, category, (x1, y1 - 25), 0, 0.7, (0, 255, 0), 2)
+
+        line = f'{now}, {x1:3d}, {y1:3d}, {x2:3d}, {y2:3d}, {obj_score:0.2f}, {int(box[5]):2d}, {category}\n'
+        # print(line)
+        log_file.write(line)
+    return frame
+
+
+def yolov7_onnx_process(frame, size='tiny_256x320'):
+    global models
+
+    model_name = f'yolov7-{size}.onnx'
+    # load pretrained model
+    if model_name in models:
+        model = models[model_name]
+    else:
+        model_path = f"models/{model_name}"
+        model = YOLOv7(model_path, conf_thres=0.5, iou_thres=0.5)
+
+        models[model_name] = model
+
+    # Update object localizer
+    boxes, scores, class_ids = model(frame)
+    combined_img = model.draw_detections(frame)
+
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    for i in range(len(class_ids)):
+        x1, y1, x2, y2 = boxes[i]
+        score = scores[i]
+        class_id = class_ids[i]
+        line = f'{now}, {int(x1):3d}, {int(y1):3d}, {int(x2):3d}, {int(y2):3d}, {score:0.2f}, {class_id:2d}, {yolov7_names[class_id]}\n'
+        # print(line)
+        log_file.write(line)
+    return combined_img
